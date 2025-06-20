825eb07ede51d116d38df66b0d2f0b94922cc746
jenkins <jenkins@hellodevops.click>
2025-06-21 02:29:28 +0700

    change auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,11 +7,11 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
-  userAttributes: {
-    preferredUsername: {
-      required: true,
-      mutable: true
-    }
   }
+  // userAttributes: {
+  //   preferredUsername: {
+  //     required: true,
+  //     mutable: true
+  //   }
+  // }
 });
